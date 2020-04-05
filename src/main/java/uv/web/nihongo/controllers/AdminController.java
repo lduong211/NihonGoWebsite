9348ed6fe0c72a098a6a9d46d996490062755a99
ngocnhan2010 <tranngoctnn2010@gmail.com>
2020-04-05 23:40:20 +0700

    Deploy Branch

--- src/main/java/uv/web/nihongo/controllers/AdminController.java
+++ src/main/java/uv/web/nihongo/controllers/AdminController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class AdminController {
 
-    @GetMapping(value="/login")
-    public String getLoginPage() {
-        return "login";
+    @GetMapping(value = "/admin")
+    public String getUserListPage() {
+        return "admin/userList";
     }
-    
+
 }
