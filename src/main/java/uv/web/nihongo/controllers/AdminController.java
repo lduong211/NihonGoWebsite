8db1040f9ab3e81dd8c6aaf3e6270e6dc06a8bb2
duong <lduong211@gmail.com>
2020-04-13 21:29:12 +0700

    new fix

--- src/main/java/uv/web/nihongo/controllers/AdminController.java
+++ src/main/java/uv/web/nihongo/controllers/AdminController.java
@@ -1,10 +1,12 @@
 package uv.web.nihongo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+
 
     @GetMapping(value = "/admin")
     public String getUserListPage() {
--- src/main/java/uv/web/nihongo/entities/Book.java
+++ src/main/java/uv/web/nihongo/entities/Book.java
@@ -5,22 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Admin {
+public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String username;
-    private String password;
-    private String name;
-    private String roles;
+    private String nameBook; 
 
 }
