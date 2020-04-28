b624e4037adb63326f8a21fe06c00424038d5381
ngocnhan2010 <tranngoctnn2010@gmail.com>
2020-04-28 09:37:17 +0700

    Demo fragments

--- src/main/java/uv/web/nihongo/repositories/AdminRepo.java
+++
@@ -1,8 +0,0 @@
-package uv.web.nihongo.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import uv.web.nihongo.entities.Admin;
-
-public interface AdminRepo extends CrudRepository<Admin, Integer> {
-    Admin findByUsername(String username);
-}
