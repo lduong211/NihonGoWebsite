3c8daed487c9c2812885638a78d30441d96da33c
duong <lduong211@gmail.com>
2020-07-29 09:31:29 +0700

    up happyo and weekly words from database

--- src/main/java/uv/web/nihongo/controllers/HappyoController.java
+++ src/main/java/uv/web/nihongo/controllers/HappyoController.java
@@ -1,14 +1,26 @@
 package uv.web.nihongo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import uv.web.nihongo.repositories.HappyoRepo;
 
 @Controller
 public class HappyoController {
 
-    @GetMapping(value="/happyo")
-    public String getHappyoPage() {
+    @Autowired
+    HappyoRepo happyoRepo;
+    
+    // @GetMapping(value="/happyo")
+    // public String getHappyoPage() {
+    //     return "happyo";
+    // }
+
+    @GetMapping("/happyo")
+    private String getHappyos(Model model) {
+        model.addAttribute("happyos", happyoRepo.findAll());
         return "happyo";
     }
-
 }
--- src/main/java/uv/web/nihongo/controllers/WeeklyWordsController.java
+++ src/main/java/uv/web/nihongo/controllers/WeeklyWordsController.java
@@ -1,14 +1,27 @@
 package uv.web.nihongo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import uv.web.nihongo.repositories.WeeklyWordRepo;
 
 @Controller
 public class WeeklyWordsController {
 
-    @GetMapping(value="/weekly-words")
-    public String getUnitsPage() {
+    @Autowired
+    WeeklyWordRepo weeklyWordRepo;
+
+    @GetMapping("/weekly-words")
+    private String getWeeklyWords(Model model) {
+        model.addAttribute("weeklywords", weeklyWordRepo.findAll());
         return "weekly-words";
     }
+
+    // @GetMapping(value="/weekly-words")
+    // public String getUnitsPage() {
+    //     return "weekly-words";
+    // }
     
 }
