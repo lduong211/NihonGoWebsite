4947a6f8804e9571f3068d47249e0862e6fbdc29
duong <lduong211@gmail.com>
2020-08-06 23:59:54 +0700

    new UI

--- src/main/java/uv/web/nihongo/controllers/HappyoController.java
+++ src/main/java/uv/web/nihongo/controllers/HappyoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import uv.web.nihongo.repositories.HappyoRepo;
 
@@ -13,14 +14,21 @@ public class HappyoController {
     @Autowired
     HappyoRepo happyoRepo;
     
-    // @GetMapping(value="/happyo")
-    // public String getHappyoPage() {
+    @GetMapping("/listhappyo")
+    private String getHappyoPage(Model model) {
+        model.addAttribute("happyos", happyoRepo.findAll());
+        return "listhappyo";
+    }
+
+    // @GetMapping("/happyo")
+    // private String getHappyos(Model model) {
+    //     model.addAttribute("happyos", happyoRepo.findAll());
     //     return "happyo";
     // }
 
-    @GetMapping("/happyo")
-    private String getHappyos(Model model) {
-        model.addAttribute("happyos", happyoRepo.findAll());
+    @GetMapping("happyo/{id}")
+    private String editHappyo(@PathVariable int id, Model model) {
+        model.addAttribute("happyo", happyoRepo.findById(id).get());
         return "happyo";
     }
 }
